b008ed613075900221982863d3c9ac4241718506
dan800 <marsh@ucar.edu>
2022-07-11 14:02:49 -0600

    Created SWIM directory. Added util.py and climate_models.py

---
+++ SWIM/climate_models.py
@@ -0,0 +1,84 @@
+import os
+import numpy as np
+import xarray as xr
+
+# function to read in a climate model solar spectral irradiance and its grid
+# input: 
+#    model name
+# output:
+#    irradiance (units: mW/m2)
+#    grid (units: nm)
+#    name of stellar object
+
+def model_spectrum(model):
+
+    #certain exoplanet/climate models require different files types
+
+    unavailable = False
+    
+    if (model == 'WACCM' or model == 'CAM'):
+        solar_file = 'SolarForcingCMIP6piControl_c160921.nc'
+        #check for file. Download if not in directory
+        if(os.path.isfile(solar_file)): 
+            print('File already downloaded')
+        else: 
+            url = 'https://svn-ccsm-inputdata.cgd.ucar.edu/trunk/inputdata/atm/cam/solar/'
+            os.system('wget '+url+solar_file+' --no-check-certificate')
+    
+        #read in file
+        ds = xr.open_dataset(solar_file)#attach file to dataset
+        ssi = ds['ssi'].isel(time=0) #define dataset from file
+        model_flux = ssi.values #flux values
+        model_wavelength = ssi.wavelength.values #wavelength grid
+        starlabel = 'Sun' #label for plot
+    
+    elif(model == 'ExoCAM'):
+        print('ExoCam spectrum input file unknown')
+        unavailable = True
+    
+    elif(model == 'UM'):
+        print('UM spectrum input file unknown')
+        unavailable = True
+    
+    elif(model == 'LMD-G'):
+        print('LMD-G spectrum input file unknown')
+        unavailable = True
+    
+    elif(model == 'ROCKE-3D'):
+        #check for file. Download if not in directory
+        if(os.path.isfile('sun')): 
+            print('File already downloaded')
+        else: 
+            os.system('wget https://portal.nccs.nasa.gov/GISS_modelE/ROCKE-3D/stellar_spectra/sun')
+        
+        # note Wavelength is in m, Irradiance is (W/m3)
+        ROCKE_3D_solar_input = np.genfromtxt('sun', skip_header = 5, skip_footer = 1)
+        model_flux = ROCKE_3D_solar_input[:,1]*1e-3 # (mW/m3)
+        model_wavelength =  ROCKE_3D_solar_input[:,0]*1e9 # wavelength grid to nm
+        starlabel = 'Sun' #label for plot
+    
+    elif(model == 'CCSM3'):
+        print('CCSM3 spectrum input file unknown')
+        unavailable = True
+    
+    elif(model == 'Atmos'):
+        solar_file = 'muscles_gj667c.txt'  
+        if(os.path.isfile(solar_file)): 
+            print('File already downloaded')
+        else:
+            url = 'https://raw.githubusercontent.com/VirtualPlanetaryLaboratory/atmos/master/PHOTOCHEM/DATA/FLUX/'
+            os.system('wget '+url+solar_file)
+        ATMOS_gj667c_input = np.genfromtxt(solar_file)
+        model_wavelength = ATMOS_gj667c_input[:,0]*0.1
+        model_flux = ATMOS_gj667c_input[:,1]
+        starlabel = 'GJ 667 C'
+    
+    else:
+        print('Spectrum input file unknown')
+        
+    if  unavailable:
+        model_flux = 0.0
+        model_wavelength = 0.0
+        starlabel = 'n/a'
+
+    return model_flux, model_wavelength, starlabel
---
+++ docs/day1sum.md
@@ -0,0 +1,51 @@
+### 3rd Eddy Symposium (Exo)Planetary Atmosphere Working Group Day 1 Summary ###
+
+**Participants (Total 10):**
+
+Ankush Bhaskar, Space Physics Laboratory, VSSC, ISRO, India
+
+Charles Bowers, University of Michigan Department of Climatology and Space Sciences and Engineering
+
+Greg Cooke, University of Leeds
+
+Caitlin Gough, University of Leeds
+
+Meng Jin, Lockheed Martin Solar & Astrophysics Lab / SETI Institute
+
+Philip Judge, HAO/NCAR
+
+*Dan Marsh, NCAR
+
+Keshav R. Tripathi, Space Physics Laboratory, VSSC, ISRO, India
+
+Allison Youngblood, NASA Goddard Space Flight Center
+
+Jamila Chouqar, Cadi Ayad University
+
+*Contributing ideas
+
+**Main Agenda:**
+
+1. We went around the table and introduced our research interests, personal objectives for this symposium;
+
+2. Brain-storming ideas for the working group topics.
+
+**Summary:**
+
+We generated 8 potential topics to work with:
+
+1. **Create new open source tools to create solar/stellar spectral irradiance files for use in planetary modeling.** Access online irradiance catalogs and create netcdf files on user defined spectral grid. Different one-dimensional and three-dimensional atmospheric models have different spectral grids. We want to create an easy to use python script to download stellar spectra from various sources and scale the total stellar irradiance to match the irradiance that known exoplanets would receive. Also conserve flux in each wavelength and map it onto different grids. Then 1D and 3D modellers (and potentially other researchers) can use these stellar input for modelling - and it will make it easier to do intercomparions because you are using the same spectra. 
+
+2. **Create open source pipeline to take atmospheric output from various models and use it for PandExo/NASA PSG/Other spectroscopic suites.**
+
+3. **Create stellar observational database for validating/constraining stellar models.** When adapting solar models to stellar studies, one major difficulty is the limited stellar observations without spatial resolution, which provides less constraint on the stellar wind and CME models. It is important to have a open access database to sumerize useful stellar observations (e.g., magnetic field, UV/X-ray emissions etc.)
+
+4. **The role of EUV radiation in driving atmospheric escape**: "Knowledge of EUV radiation from stars in general is currently guesswork and uncertain by factors of 10 or more. We do not know whether atmospheres survive on other planets." - NExtUP website. **What tools do we need to evaluate this uncertainty?**
+
+5. **Create wishlist of stellar or exoplanet observations for exoplanet models.**
+
+6. **Using known stable radio source to probe the atmosphere of planet (Stellar occultation).**
+
+7. **Create an extensive publicly available library of simulations covering fine varitions of models parameters for exoplanets particularly super earths and sub neptunes.**
+
+8. **Work together on a draft JWST observation proposal.**
