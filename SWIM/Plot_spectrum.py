714e900a7c1f6ce5124a319363f91deebd262d79
G J Cooke <pygjc@login2.arc4.leeds.ac.uk>
2022-07-13 15:52:24 +0100

    Updating plotting function

--- SWIM/Plot_spectrum.py
+++ SWIM/Plot_spectrum.py
@@ -3,8 +3,11 @@ import numpy as np
 import xarray as xr
 
 # function to plot a spectrum
+# can choose to plot 1 or 2 by specifying the number - more can easily be added if required
+# labels are for the legend
+# ylog = True sets yscale to a log scale
 
-def plot_spectrum(wav1, flux1, wav2,  flux2, number = 1, 
+def plot_spectrum(wav1, flux1, wav2, flux2, number = 1, 
                   flux_units = '(erg/cm2/s/Ang)', label1 = '', label2 = '', ylog = True, xlim = (1,1e5)):
 
     plt.figure(figsize = (12,5))
